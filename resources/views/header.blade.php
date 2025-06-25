e22ef1e4922fddaece8fc0c376f763f48f5eb2cd
yechika <mantuldanil@gmail.com>
2025-06-25 17:10:14 +0700

    ubah tipis

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Byonlab - Distributor Resmi Thermo Fisher Scientific</title>
+    <title>Byonlab</title>
     <base target="_self">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/@preline/preline@2.0.0/dist/preline.min.js"></script>
