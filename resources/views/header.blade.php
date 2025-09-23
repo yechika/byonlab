adfa30385fd1f9a76ae9389ef75acfc06751d908
yechika <mantuldanil@gmail.com>
2025-09-23 19:11:19 +0700

    tambah icon

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Byonlab</title>
+    <title>PT Biondi Loka Niaga</title>
     <base target="_self">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/@preline/preline@2.0.0/dist/preline.min.js"></script>
@@ -9,6 +9,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link rel="icon" href="{{ asset('storage/logo_biondi.png') }}" type="image/png">
+
     <script>
         tailwind.config = {
             darkMode: 'class',
