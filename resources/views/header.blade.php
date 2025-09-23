6d236f9f8e617e1697874a6c34b0033edce6f0eb
yechika <mantuldanil@gmail.com>
2025-09-23 19:13:19 +0700

    update icon

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link rel="icon" href="{{ asset('storage/1000067783-removebg-preview.png') }}" type="image/png">
     <script>
         tailwind.config = {
             darkMode: 'class',
